d3fb897852a9cec66f1d56d8b4f2bc0014adeaf8
shivamlodhi <54970456+shivamlodhi@users.noreply.github.com>
2020-10-06 09:43:30 +0530

    Update Cat.java

--- src/com/javarush/test/level05/lesson05/task01/Cat.java
+++ src/com/javarush/test/level05/lesson05/task01/Cat.java
@@ -2,6 +2,8 @@ package com.javarush.test.level05.lesson05.task01;
 
 /* Создать класс Cat
 Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
+
+the cat class made for cate age weight ans strength
 */
 
 public class Cat {
